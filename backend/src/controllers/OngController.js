b68abd57086bf502974536edf8905f8baacc4237
RaphaCosta-Lab <rafa.costa0@hotmail.com>
2020-03-27 10:34:23 -0300

    Testes automatizados

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
 const connection = require('../database/connectino');
-const crypto = require('crypto');
+const generateUniqueId = require('../../utils/generateUniqueId');
 
 module.exports = {
   async index(request, response) {
@@ -12,7 +12,7 @@ module.exports = {
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
