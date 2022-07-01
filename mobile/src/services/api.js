8f9ecd9bd1366c4c2680247a4a5503d625811c75
raphaacosta <rafa.costa0@hotmail.com>
2022-06-30 23:23:10 -0300

    atualiza dependencias e expo

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://mysterious-earth-86666.herokuapp.com/'
+  baseURL: 'http://10.0.0.101:3333/'
 });
 
 export default api;
